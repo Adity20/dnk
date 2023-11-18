6f0521050eef360195cfa9430eebe12c162686d0
Aditya <sharma.aditya9164@gmail.com>
2023-11-18 14:42:44 +0530

    changed some files and added some

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
 export default function App() {
-  return (
-    <h1 className='text-red-800'>App</h1>
-  )
+  return <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/signin" element={<SignIn />} />
+    <Route path="/signup" element={<SignUp />} />
+    <Route path="/profile" element={<Profile />} />
+  </Routes>
+  </BrowserRouter>
 }
