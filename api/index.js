26470c96372431e36b1802a7efdb1e7cdab2b23d
Aditya <sharma.aditya9164@gmail.com>
2023-11-18 20:05:10 +0530

    added .env file and mongoose connection

--- api/index.js
+++ api/index.js
@@ -1,5 +1,13 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+    console.log('Connected to MongoDB!!!');
+}).catch((err) => {
+    console.log(err);
+});
 const app = express();
 
 app.listen(3900, () => {
