adcbc64699517bbddcc8a8d3d6858e277fb112b3
Aditya <sharma.aditya9164@gmail.com>
2023-11-20 12:49:10 +0530

    changed some files related to routing

--- api/index.js
+++ api/index.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import User from './models/user.models.js';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
+
+
 
 mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log('Connected to MongoDB!!!');
 }).catch((err) => {
-    console.log(err);
+    console.log('Connection Failed');
 });
 const app = express();
 
 app.listen(3900, () => {
     console.log('Server is running on port 3900');
     });
+
+
+app.get('/api/user',userRoutes);    
+
--- api/models/user.models.js
+++ api/models/user.models.js
@@ -15,7 +15,6 @@ const userSchema = new mongoose.Schema({
     type:String,
     required: true,
     },
-    times
 }, {timestamps: true});
 
 const User = mongoose.model('User', userSchema);
