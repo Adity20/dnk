c18ddb2accfb44b37fba1460f90d5eb7e14b8a00
Aditya Sharma <sharma.aditya9164@gmail.com>
2024-01-14 22:52:08 +0530

    created signin route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.models.js';
 import bcryptjs from 'bcryptjs';
 import {errorHandler} from '../utils/error.js';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body;
@@ -10,7 +11,24 @@ export const signup = async (req, res, next) => {
         await newUser.save();
         res.status(201).json('User Created');
     } catch (err) { 
-        next(errorHandler(500, 'Internal Server Error'));
+        next(errorHandler(err));
     }
 };
+
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body;
+    try {
+        const ValidUser = await User.findOne({email});
+        if(!ValidUser) return next(errorHandler(401, 'Email or Password is wrong'));
+        const validPassword = bcryptjs.compareSync(password, ValidUser.password);
+        if(!validPassword) return next(errorHandler(401, 'Email or Password is wrong'));
+        const token = jwt.sign({id: ValidUser._id}, process.env.JWT_SECRET, {expiresIn: '1d'});
+        res.cookie('token', token, {httpOnly: true});
+        res.status(200).json('User Logged');
+        const {password: pass, ...user} = ValidUser._doc;
+    } catch (error) {
+        next(errorHandler(error))
+    }
+}
 
